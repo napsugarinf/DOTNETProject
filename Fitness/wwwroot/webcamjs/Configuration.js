34b232ce1b954440652bcb920a297d7d3ec633da
Portik-Andrea <portik.andrea@student.ms.sapientia.ro>
2023-05-03 19:52:19 +0300

    add webcam

---
+++ Fitness/wwwroot/webcamjs/Configuration.js
@@ -0,0 +1,22 @@
+﻿function ready() {
+    if (document.readyState == 'complete') {
+        Webcam.set({
+            with: 280,
+            height: 280,
+            image_format: 'jpeg',
+            jpeg_quality: 90
+        });
+        try {
+            Webcam.attach('#camera');
+        } catch (e) {
+            alert(e);
+        }
+    }
+}
+function take_snapshot() {
+    var data = null;
+    Webcam.snap(function (data_uri) {
+        data = data_uri;
+    });
+    return data;
+}
